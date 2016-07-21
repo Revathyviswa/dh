233ade732763bdd15dfc7c0530b2f8526f0ec171
Revathyviswa <hemarev05@gmail.com>
2016-07-21 15:18:16 +0530

    Add files via upload

---
+++ JavaApplication7.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication7 {
+public static int convert_String_To_Number(String numStr){
+         
+        char ch[] = numStr.toCharArray();
+        int sum = 0;
+        //get ascii value for zero
+        int zeroAscii = (int)'0';
+        for(char c:ch){
+            int tmpAscii = (int)c;
+            sum = (sum*10)+(tmpAscii-zeroAscii);
+        }
+        return sum;}
+    public static void main(String[] args) {
+        System.out.println("\"3256\" == "+convert_String_To_Number("3256"));
+        System.out.println("\"76289\" == "+convert_String_To_Number("76289"));
+        System.out.println("\"90087\" == "+convert_String_To_Number("90087"));
+    }
+}
